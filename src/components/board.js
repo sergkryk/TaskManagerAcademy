fbae9e1047fb1629d0fe0a9460375887ffca4b01
Sasha Sushko <sushko@outlook.com>
2019-12-02 16:21:31 +0300

    4.1.4 Перепишет на классы список задач

--- src/components/board.js
+++ src/components/board.js
@@ -1,4 +1,6 @@
-export const createBoardTemplate = () => {
+import {createElement} from '../utils.js';
+
+const createBoardTemplate = () => {
   return (
     `<section class="board container">
         <div class="board__filter-list">
@@ -11,3 +13,26 @@ export const createBoardTemplate = () => {
       </section>`
   );
 };
+
+
+export default class Board {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createBoardTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
