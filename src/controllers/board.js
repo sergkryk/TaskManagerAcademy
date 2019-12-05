193e4a8bbd6cc0c4abcbaa5ff62f6ab7ded5766d
Sasha Sushko <sushko@outlook.com>
2019-12-05 19:56:43 +0300

    5.1.22 Перенесет зависимости

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -1,3 +1,11 @@
+import LoadMoreButtonComponent from '../components/load-more-button.js';
+import TaskEditComponent from '../components/task-edit.js';
+import TaskComponent from '../components/task.js';
+import TasksComponent from '../components/tasks.js';
+import SortComponent from '../components/sort.js';
+import NoTasksComponent from '../components/no-tasks.js';
+import {render, remove, replace, RenderPosition} from '../utils/render.js';
+
 const renderTask = (taskListElement, task) => {
   const onEscKeyDown = (evt) => {
     const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
