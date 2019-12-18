82aa39990955ad510bebc8dd73dc8b0b3c966333
Sasha Sushko <sushko@outlook.com>
2019-12-18 20:09:27 +0300

    7.2.20 По аналогии создаст функции для отрисовки оставшихся графиков

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -1,4 +1,5 @@
 import AbstractSmartComponent from './abstract-smart-component.js';
+import {isOneDay} from '../utils/common.js';
 
 const Color = {
   BLACK: `black`,
@@ -103,6 +104,148 @@ const renderColorsChart = (colorsCtx, tasks) => {
       title: {
         display: true,
         text: `DONE BY: COLORS`,
+        fontSize: 16,
+        fontColor: `#000000`
+      },
+      legend: {
+        position: `left`,
+        labels: {
+          boxWidth: 15,
+          padding: 25,
+          fontStyle: 500,
+          fontColor: `#000000`,
+          fontSize: 13
+        }
+      }
+    }
+  });
+};
+
+const renderDaysChart = (daysCtx, tasks, dateFrom, dateTo) => {
+  const days = calculateBetweenDates(dateFrom, dateTo);
+
+  const taskCountOnDay = days.map((date) => {
+    return tasks.filter((task) => {
+      return isOneDay(task.dueDate, date);
+    }).length;
+  });
+
+  const formattedDates = days.map((it) => window.moment(it).format(`DD MMM`));
+
+  return new window.Chart(daysCtx, {
+    plugins: [window.ChartDataLabels],
+    type: `line`,
+    data: {
+      labels: formattedDates,
+      datasets: [{
+        data: taskCountOnDay,
+        backgroundColor: `transparent`,
+        borderColor: `#000000`,
+        borderWidth: 1,
+        lineTension: 0,
+        pointRadius: 8,
+        pointHoverRadius: 8,
+        pointBackgroundColor: `#000000`
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 8
+          },
+          color: `#ffffff`
+        }
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            display: false
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          }
+        }],
+        xAxes: [{
+          ticks: {
+            fontStyle: `bold`,
+            fontColor: `#000000`
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      },
+      layout: {
+        padding: {
+          top: 10
+        }
+      },
+      tooltips: {
+        enabled: false
+      }
+    }
+  });
+};
+
+const renderTagsChart = (tagsCtx, tasks) => {
+  const tagsLabels = tasks.map((task) => task.tags)
+    .reduce((acc, tags) => {
+      return acc.concat(Array.from(tags));
+    }, [])
+    .filter(getUniqItems);
+
+  return new window.Chart(tagsCtx, {
+    plugins: [window.ChartDataLabels],
+    type: `pie`,
+    data: {
+      labels: tagsLabels,
+      datasets: [{
+        data: tagsLabels.map((tag) => tasks.reduce((acc, task) => {
+          const targetTasksCount = Array.from(task.tags)
+            .filter((it) => it === tag).length;
+
+          return acc + targetTasksCount;
+        }, 0)),
+        backgroundColor: tagsLabels.map(createRandomColor)
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          display: false
+        }
+      },
+      tooltips: {
+        callbacks: {
+          label: (tooltipItem, data) => {
+            const allData = data.datasets[tooltipItem.datasetIndex].data;
+            const tooltipData = allData[tooltipItem.index];
+
+            const total = allData.reduce((acc, it) => acc + parseFloat(it));
+            const tooltipPercentage = Math.round((tooltipData / total) * 100);
+
+            return `${tooltipData} TASKS — ${tooltipPercentage}%`;
+          }
+        },
+        displayColors: false,
+        backgroundColor: `#ffffff`,
+        bodyFontColor: `#000000`,
+        borderColor: `#000000`,
+        borderWidth: 1,
+        cornerRadius: 0,
+        xPadding: 15,
+        yPadding: 15
+      },
+      title: {
+        display: true,
+        text: `DONE BY: TAGS`,
         fontSize: 16,
         fontColor: `#000000`
       },
