c9282105a8e21af1cc1690e7f01f6c67c6b1c43a
Sasha Sushko <sushko@outlook.com>
2019-12-18 21:16:25 +0300

    8.1.3 Опишет вспомогательную функцию для проверки статуса ответа

--- src/api.js
+++ src/api.js
@@ -5,6 +5,14 @@ const Method = {
   DELETE: `DELETE`
 };
 
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
 const API = class {
 };
 
