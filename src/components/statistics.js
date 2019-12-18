1e4fe576117e24d1c459ca7e634b687bdb8e5706
Sasha Sushko <sushko@outlook.com>
2019-12-18 19:54:13 +0300

    7.2.13 Научит шаблон экрана статистики принимать данные снаружи

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -1,6 +1,8 @@
 import AbstractSmartComponent from './abstract-smart-component.js';
 
-const createStatisticsTemplate = () => {
+const createStatisticsTemplate = ({tasks, dateFrom, dateTo}) => {
+  const placeholder = createPlaceholder(dateFrom, dateTo);
+  const tasksCount = getTasksByDateRange(tasks, dateFrom, dateTo).length;
   return (
     `<section class="statistic container">
       <div class="statistic__line">
@@ -8,24 +10,24 @@ const createStatisticsTemplate = () => {
           <h2 class="statistic__period-title">Task Activity DIAGRAM</h2>
 
           <div class="statistic-input-wrap">
-            <input class="statistic__period-input" type="text" placeholder="01 Feb - 08 Feb">
+            <input class="statistic__period-input" type="text" placeholder="${placeholder}">
           </div>
 
           <p class="statistic__period-result">
             In total for the specified period
-            <span class="statistic__task-found">0</span> tasks were fulfilled.
+            <span class="statistic__task-found">${tasksCount}</span> tasks were fulfilled.
           </p>
         </div>
-        <div class="statistic__line-graphic visually-hidden">
+        <div class="statistic__line-graphic">
           <canvas class="statistic__days" width="550" height="150"></canvas>
         </div>
       </div>
 
       <div class="statistic__circle">
-        <div class="statistic__tags-wrap visually-hidden">
+        <div class="statistic__tags-wrap">
           <canvas class="statistic__tags" width="400" height="300"></canvas>
         </div>
-        <div class="statistic__colors-wrap visually-hidden">
+        <div class="statistic__colors-wrap">
           <canvas class="statistic__colors" width="400" height="300"></canvas>
         </div>
       </div>
