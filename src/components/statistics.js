4de079a0d3c10f267d912dca639867b0af3065a1
Sasha Sushko <sushko@outlook.com>
2019-12-18 19:59:26 +0300

    7.2.15 Добавит вспомогательные функции для отрисовки графиков

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -16,6 +16,49 @@ const ColorValue = {
   [Color.YELLOW]: `#ffe125`,
 };
 
+const getUniqItems = (item, index, array) => {
+  return array.indexOf(item) === index;
+};
+
+const getTasksByDateRange = (tasks, dateFrom, dateTo) => {
+  return tasks.filter((task) => {
+    const dueDate = task.dueDate;
+
+    return dueDate >= dateFrom && dueDate <= dateTo;
+  });
+};
+
+const createRandomColor = () => {
+  const value = Math.floor(Math.random() * 0xffffff);
+
+  return `#${value.toString(16)}`;
+};
+
+const createPlaceholder = (dateFrom, dateTo) => {
+  const format = (date) => {
+    return window.moment(date).format(`DD MMM`);
+  };
+
+  return `${format(dateFrom)} - ${format(dateTo)}`;
+};
+
+const calcUniqCountColor = (tasks, color) => {
+  return tasks.filter((it) => it.color === color).length;
+};
+
+const calculateBetweenDates = (from, to) => {
+  const result = [];
+  let date = new Date(from);
+
+  while (date <= to) {
+    result.push(date);
+
+    date = new Date(date);
+    date.setDate(date.getDate() + 1);
+  }
+
+  return result;
+};
 const createStatisticsTemplate = ({tasks, dateFrom, dateTo}) => {
   const placeholder = createPlaceholder(dateFrom, dateTo);
   const tasksCount = getTasksByDateRange(tasks, dateFrom, dateTo).length;
