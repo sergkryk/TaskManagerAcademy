038ee59633a1148e5fc50e81dfb70bd9e296b252
Sasha Sushko <sushko@outlook.com>
2019-12-18 20:07:39 +0300

    7.2.18 Создаст новый график по заданным настройкам
    Эти настройки можно просто скопировать. Подробнее о них можно почитать на сайте Chart.js, ссылка есть в домашнем задании

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -65,6 +65,52 @@ const renderColorsChart = (colorsCtx, tasks) => {
     .map((task) => task.color)
     .filter(getUniqItems);
 
+  return new window.Chart(colorsCtx, {
+    plugins: [window.ChartDataLabels],
+    type: `pie`,
+    options: {
+      plugins: {
+        datalabels: {
+          display: false
+        }
+      },
+      tooltips: {
+        callbacks: {
+          label: (tooltipItem, data) => {
+            const allData = data.datasets[tooltipItem.datasetIndex].data;
+            const tooltipData = allData[tooltipItem.index];
+            const total = allData.reduce((acc, it) => acc + parseFloat(it));
+            const tooltipPercentage = Math.round((tooltipData / total) * 100);
+            return `${tooltipData} TASKS — ${tooltipPercentage}%`;
+          }
+        },
+        displayColors: false,
+        backgroundColor: `#ffffff`,
+        bodyFontColor: `#000000`,
+        borderColor: `#000000`,
+        borderWidth: 1,
+        cornerRadius: 0,
+        xPadding: 15,
+        yPadding: 15
+      },
+      title: {
+        display: true,
+        text: `DONE BY: COLORS`,
+        fontSize: 16,
+        fontColor: `#000000`
+      },
+      legend: {
+        position: `left`,
+        labels: {
+          boxWidth: 15,
+          padding: 25,
+          fontStyle: 500,
+          fontColor: `#000000`,
+          fontSize: 13
+        }
+      }
+    }
+  });
 };
 
 const createStatisticsTemplate = ({tasks, dateFrom, dateTo}) => {
