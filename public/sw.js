db10955b11365b4c178bb7f3c11863681a427493
Sasha Sushko <sushko@outlook.com>
2019-12-24 13:27:00 +0300

    9.1.2 Закэширует файлы при регистрации ServiceWorker
    По событию install, которое означает, что ServiceWorker успешно зарегистрирован и устанавливается, добавим в кэш наши статичные файлы.
    
    При первой загрузке ServiceWorker сохранить их в кэш браузера, после мы сможем отдавать их пользователю из кэша, а не загружать повторно.

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,30 @@
+const CACHE_PREFIX = `taskmanager-cache`;
+const CACHE_VER = `v1`;
+const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VER}`;
+
 self.addEventListener(`install`, (evt) => {
+  evt.waitUntil(
+      caches.open(CACHE_NAME)
+        .then((cache) => {
+          return cache.addAll([
+            `/`,
+            `/index.html`,
+            `/bundle.js`,
+            `/css/normalize.css`,
+            `/css/style.css`,
+            `/fonts/HelveticaNeueCyr-Bold.woff`,
+            `/fonts/HelveticaNeueCyr-Bold.woff2`,
+            `/fonts/HelveticaNeueCyr-Medium.woff`,
+            `/fonts/HelveticaNeueCyr-Medium.woff2`,
+            `/fonts/HelveticaNeueCyr-Roman.woff`,
+            `/fonts/HelveticaNeueCyr-Roman.woff2`,
+            `/img/add-photo.svg`,
+            `/img/close.svg`,
+            `/img/sample-img.jpg`,
+            `/img/wave.svg`,
+          ]);
+        })
+  );
 });
 
 self.addEventListener(`activate`, (evt) => {
